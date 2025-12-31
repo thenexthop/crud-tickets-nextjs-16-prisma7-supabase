a6308c2fc4fec555f6f85a80bfba2644978c9706
alfredoh <alfredoh.ucv@gmail.com>
2025-12-31 10:05:45 -0400

    cambios en el next config para intentar hacer el Deploy en Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
+    '/*': ['./node_modules/.prisma/client/**/*'],
+  },
 };
 
 export default nextConfig;
